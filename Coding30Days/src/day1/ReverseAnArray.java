be2f6e1d12e34e4ccca4b49d2305393d643ba874
subhashmungare <subhashmungare17@gmail.com>
2023-05-23 00:58:49 +0530

    code

--- Coding30Days/src/day1/ReverseAnArray.java
+++ Coding30Days/src/day1/ReverseAnArray.java
@@ -6,25 +6,52 @@ import java.util.Scanner;
 public class ReverseAnArray {
 
 	public static void main(String[] args) {
+	
 		Scanner sc=new Scanner(System.in);
+		System.out.println(" enter number of values...");
+		int num=sc.nextInt();
+		int sm[]=new int[num];
+		System.out.println("Enter a value");
+		for (int i = 0; i < sm.length; i++) {
+			sm[i]=sc.nextInt();
 		
-		int array=sc.nextInt();
-		
-		int s[]= {1,2,3,4,5};
-		int i=0;
-		int j=s.length-1;
-		
+		int a=0;
+		int j=sm.length-1;
 		int temp;
-		while(i<j) {
-			temp=s[i];
-			s[i]=s[j];
-			s[j]=temp;
-			i++;
+		while(a<j) {
+			temp=sm[a];
+			sm[a]=sm[j];
+			sm[j]=temp;
+			a++;
 			j--;
 		}
-		for (int k = 0; k < s.length; k++) {
-			System.out.print(s[k]+ "  ");
+	}
+		for (int k = 0; k < sm.length; k++) {
+			System.out.print(sm[k]+ "  ");
 		}
+		
+		
+//		int sm[]= {7,8,9,10,11,12,13,14};
+//		int i=0;
+//		int j=sm.length-1;
+//		int temp;
+//		while(i<j) {
+//			temp=sm[i];
+//			sm[i]=sm[j];
+//			sm[j]=temp;
+//			i++;
+//			j--;
+//		}
+//		for (int k = 0; k < sm.length; k++) {
+//			System.out.print(sm[k]+" ");
+//		}
+//		
+//		
+		
+		
+		
+		
+		
 	}
 
 }
--- Coding30Days/src/day1/ReverseAnArrayList.java
+++ Coding30Days/src/day1/ReverseAnArrayList.java
@@ -1,9 +1,22 @@
 package day1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class ReverseAnArrayList {
 
 	public static void main(String[] args) {
-		
+		List<Integer>sm=new ArrayList<>();
+		sm.add(1);
+		sm.add(2);
+		sm.add(3);
+		sm.add(4);
+		sm.add(5);
+		System.out.println(sm);
+		Collections.reverse(sm);
+		System.out.println(sm);
 
 	}
 
